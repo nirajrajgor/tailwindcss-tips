02cb3346f47d2cac163b8d3d2db81bb855083917
niraj <nirajr1992@gmail.com>
2024-06-19 16:44:10 -0700

    Update style, add subtitle, add github repo link

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import "./App.css";
 import Description from "./Description";
+import Hr from "./Hr";
 import Title from "./Title";
 
 function App() {
   return (
-    <div className="container mx-auto px-4 py-12 border-2 border-rose-600">
-      <h1 className="text-4xl font-bold max-w-3xl mb-8 mx-auto text-center">
+    <div className="container mx-auto px-4 py-12">
+      <h1 className="text-4xl font-bold max-w-3xl mb-2 mx-auto text-center">
         Tips and Tricks with Tailwind CSS
       </h1>
+      <h2 className="mb-8 text-base text-gray-500 sm:text-lg max-w-3xl mx-auto text-center dark:text-gray-400">
+        Learn how to tailor your Tailwind CSS project with the help of Tailwind
+        CSS examples shown below.
+      </h2>
       <main className="grid grid-cols-1 gap-8 md:grid-cols-2">
-        <section className="bg-gray-50 p-8 rounded-2xl hover:shadow">
+        <section className="bg-gray-50 p-8 rounded-2xl">
           <Title text="1: Delegate classes to parent" />
           <Description>
-            Instead of using <b>text-2xl</b> on each li, we can use the same
-            class on the ul
+            The below nav items has <b>text-2xl</b> on ul tag instead of having
+            on each li tag. This helps to avoid writing it multiple times.
           </Description>
+          <Hr />
           <nav>
-            <ul className="flex flex-wrap text-2xl text-teal-700 text-opacity-50 p-1">
+            <ul className="flex flex-wrap text-2xl text-fuchsia-800 text-opacity-70 p-1">
               <li className="mr-4 bg-gray-200 text-blue-400 p-1">
                 <a href="#">Home</a>
               </li>
@@ -34,8 +40,9 @@ function App() {
           <Description>
             Instead of using <b>gap-*, margin, padding</b> property on each
             child, we can use the <b>space-x-*, space-y-*</b> property on the
-            parent
+            parent.
           </Description>
+          <Hr />
           <ul className="flex flex-row space-x-5">
             <li className="size-16 bg-red-400">Item 1</li>
             <li className="size-16 bg-red-500">Item 2</li>
@@ -46,11 +53,12 @@ function App() {
         <section className="bg-gray-50 p-8 rounded-2xl">
           <Title text="3: Don't overuse @apply utility classes" />
           <Description>
-            Using custom classname like{" "}
-            <code className="bg-gray-200 p-1">.nav-items</code> and adding
-            tailwind classes in css files using <b>@apply</b> can cause problems
-            like name collisions and hard to maintain code.
+            Using custom classname like <b>.nav-items</b> and adding tailwind
+            classes in css files using <b>@apply</b> can cause problems like
+            name collisions and hard to maintain code. It is ok to use it when
+            working with third party libraries.
           </Description>
+          <Hr />
           <ul className="nav-items">
             <li>Item 1</li>
             <li>Item 2</li>
@@ -61,20 +69,20 @@ function App() {
           <Title text="4: Adding opacity to background & text" />
           <Description>
             You can apply opacity to background and text using{" "}
-            <code className="bg-gray-200 p-1">
-              .bg-opacity-* & .text-opacity-*{" "}
-            </code>
+            <b>.bg-opacity-*</b> and <b>.text-opacity-*</b> tailwind classes.
           </Description>
-          <div className="bg-teal-700 bg-opacity-50 w-[200px]">
-            <p className="text-blue-700 text-opacity-70">tailwind css text</p>
+          <Hr />
+          <div className="bg-teal-500 bg-opacity-100 w-[200px]">
+            <p className="text-blue-900 text-opacity-70">tailwind css text</p>
           </div>
         </section>
         <section className="bg-gray-50 p-8 rounded-2xl">
           <Title text="5: Adding Transition & Animation " />
           <Description>
-            <code className="bg-gray-200 p-1">transition-* & duration-*</code>
-            property to be used
+            To animate elements <b>transition-*</b> and <b>duration-* </b>
+            classes can be used. Tailwind also has predefined animations.
           </Description>
+          <Hr />
           <button className="p-2 text-white rounded bg-blue-500 hover:bg-blue-700 active:bg-blue-900 transition-colors duration-200">
             Buy Items
           </button>
@@ -82,10 +90,12 @@ function App() {
         <section className="bg-gray-50 p-8 rounded-2xl">
           <Title text="6: Group hover example" />
           <Description>
-            Hover on button will rotate the icon present inside the button, this
-            can be done using group classname on button and group-hover:property
-            classname on child element
+            Hover on button, will rotate the icon present inside the button as a
+            separate child element. This can be achieved by adding <b>group</b>{" "}
+            classname on parent and <b>group-hover:property</b> classname on
+            child element.
           </Description>
+          <Hr />
           <button className="p-2 text-white rounded group bg-blue-500 hover:bg-blue-700 active:bg-blue-900 transition-colors duration-200">
             <span>Click!</span>
             <span className="ml-2 inline-block group-hover:rotate-90 duration-300">
@@ -96,18 +106,37 @@ function App() {
         <section className="bg-gray-50 p-8 rounded-2xl">
           <Title text="7: Using pseudo classes" />
           <Description>
-            The below button background is added using after pseudo class
+            The below button has a background color added using <b>after</b>{" "}
+            pseudo class. One can also use <b>before</b> pseudo classes the same
+            way.
           </Description>
+          <Hr />
           <button className="p-2 relative after:absolute after:bg-green-300 after:inset-0 after:z-0">
             <h6 className="z-10 relative">BUTTON </h6>
           </button>
         </section>
         <section className="bg-gray-50 p-8 rounded-2xl">
           <Title text="8: Adding Custom input values" />
-          <Description>Can be added using property-[value] syntax</Description>
+          <Description>
+            The value of any classname property can be customize using
+            <b> property-[value]</b> syntax. E.g <b>text-[#007bff]</b>,{" "}
+            <b>text-[200px]</b>
+          </Description>
+          <Hr />
           <h5 className="text-[#007bff] text-[4rem]">Hello World</h5>
         </section>
       </main>
+      <footer className="mt-6 text-center">
+        Code available on{" "}
+        <a
+          href="https://github.com/nirajrajgor/tailwindcss-tips"
+          target="_blank"
+          rel="noreferrer"
+          className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          Github
+        </a>
+      </footer>
     </div>
   );
 }
